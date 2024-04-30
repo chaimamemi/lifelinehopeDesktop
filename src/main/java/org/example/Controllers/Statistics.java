0ee3187cde16158793b8bc0ee695d9569564ef76
Wissal80 <wissal.daoud@esprit.tn>
2024-04-30 12:12:33 +0100

    call method genratecode

--- src/main/java/org/example/Controllers/Statistics.java
+++ src/main/java/org/example/Controllers/Statistics.java
@@ -1,11 +1,22 @@
 package org.example.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.example.Services.ServiceMedication;
+import org.example.models.Medication;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Statistics  implements Initializable {
@@ -24,5 +35,39 @@ public class Statistics  implements Initializable {
     }
 
     private void populatePieChart() {
+
+        List<Medication> topRatedAvisList = serviceMedication.getTopRatedAvis();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+        for (Medication medication : topRatedAvisList) {
+            pieChartData.add(new PieChart.Data(Medication. getMedicalNote(), Double.parseDouble(medication.get())));
+        }
+        piechart.setData(pieChartData);
     }
+    @FXML
+    private Button goback;
+
+    @FXML
+    void gobackon(ActionEvent event) {
+        navigateToDashboard();
+    }
+    private void navigateToDashboard() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Test2.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+            // Close the current window
+            Stage currentStage = (Stage) goback.getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
