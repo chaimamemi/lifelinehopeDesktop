acbb34bb7704c793863bcebabd87a300475f7248
Wissal80 <wissal.daoud@esprit.tn>
2024-05-04 01:02:33 +0100

    image background changed

--- src/main/java/org/example/test/MainFx.java
+++ src/main/java/org/example/test/MainFx.java
@@ -17,7 +17,7 @@ public class MainFx extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/dashboard.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AjouterMedication.fxml"));
         try {
             Parent root = loader.load();
             Scene scene = new Scene(root );
